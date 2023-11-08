9eac5757ad8a90fae47ab3a86be20ab8712a3024
mario <mariomori00@gmail.com>
2023-11-08 00:10:17 -0300

    css is ready

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Cart key
+CART_SESSION_ID = 'cart'
